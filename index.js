e7185a09b23c3fcba86c8d80838edbd8dc74f069
Muhammadjon008 <113035703+Muhammadjon008@users.noreply.github.com>
2022-09-07 18:39:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Eslatib o'tamiz, siz bu saytda yoshingiz nechadaligini bilib olasiz");
+let x = prompt("Tug'ilgan yilingizni kiriting");
+let y = prompt('Hozirgi yilni kiting');
+let c = y - x;
+alert("Siz  " + c + "  yoshdasiz  Umringiz uzoq bo'lsin");
